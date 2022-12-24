a4cc4be2b68c2f8b614a6f4357debc744f70b2ce
son1122 <wongsatorn.n@hotmail.com>
2022-12-24 20:39:37 +0700

    scroll horizon in order view

--- src/component/Order/OrderView/OrderView.js
+++ src/component/Order/OrderView/OrderView.js
@@ -1,8 +1,28 @@
-
+import "./OrderView.css"
 const OrderView = () => {
+    let data = [1,11,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+    data.map(dat=>{
+        return(
+            <img style={{width:"300px"}} src={"https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg"}/>
+        )
+    })
     return (
-        <div>
+        <div className={"order-view-grid"}>
             <p>Order View</p>
+            <p>Search</p>
+            <div className={"slide-horizon"}>
+                    <img style={{width:"300px"}} src={"https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg"}/>
+                <img style={{width:"300px"}} src={"https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg"}/>
+                <img style={{width:"300px"}} src={"https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg"}/>
+                <img style={{width:"300px"}} src={"https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg"}/>
+                <img style={{width:"300px"}} src={"https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg"}/>
+                <img style={{width:"300px"}} src={"https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg"}/>
+                <img style={{width:"300px"}} src={"https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg"}/>
+                <img style={{width:"300px"}} src={"https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg"}/>
+                <img style={{width:"300px"}} src={"https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg"}/>
+            </div>
+
+            <p>Category</p>
         </div>
     );
 }
