123ed25651c0b9e175a681534ff781cf1957a11c
son1122 <wongsatorn.n@hotmail.com>
2022-12-24 22:21:58 +0700

    login page

---
+++ src/component/Login/Login.js
@@ -0,0 +1,17 @@
+import "./Login.css"
+
+const Login = () => {
+    return (
+        <div className={"login-grid"}>
+            <h2>Image</h2>
+            <h2>Username</h2>
+            <h2>Password</h2>
+            <div className={"sign-button-grid"}>
+                <p>SignUp</p>
+                <p>Login</p>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
