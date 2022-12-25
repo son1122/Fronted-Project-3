6e74ae3f0763c1dd999a6ff7c5168e4c6cdc40a8
son1122 <wongsatorn.n@hotmail.com>
2022-12-25 19:23:21 +0700

    add signup page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Route, Routes} from "react-router-dom";
 import axios from "axios";
 import Layout from "./component/Layout/Layout";
 import Login from "./component/Login/Login";
+import Signup from "./component/Signup/Signup";
 
 function App() {
   return (
@@ -12,10 +13,11 @@ function App() {
             <Route path="/order" element={<Layout/>}/>
         </Routes>
         {/*After Login Page*/}
-        {/*    <Layout />*/}
+            <Layout />
 
         {/*Login Page*/}
-            <Login/>
+        {/*    <Login/>*/}
+        {/*    <Signup/>*/}
     </div>
   );
 }
---
+++ src/component/Signup/Signup.js
@@ -0,0 +1,10 @@
+import "./Signup.css"
+const Signup = () => {
+    return (
+        <div>
+            <h2>Signup</h2>
+        </div>
+    );
+}
+
+export default Signup;
