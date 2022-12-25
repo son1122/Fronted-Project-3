23dd371f8124a03acd50a91834a14ffb158d5f9e
son1122 <wongsatorn.n@hotmail.com>
2022-12-25 23:28:16 +0700

    add state for login

--- src/App.js
+++ src/App.js
@@ -5,19 +5,49 @@ import axios from "axios";
 import Layout from "./component/Layout/Layout";
 import Login from "./component/Login/Login";
 import Signup from "./component/Signup/Signup";
+import NotFound from "./component/NotFound/NotFound";
 
 function App() {
+
+
+
+    const [login,setLogin]=useState(false)
+
+    const loginFunc = () => {
+        // console.log(localStorage.getItem("jwt"));
+        // axios.get('http://localhost:3001/fruits', {
+        //     headers: {Authorization: `Bearer ${localStorage.getItem("jwt")}`}
+        // })
+        //     .then(res => {
+        //         console.log(res.data);
+        //         setLogin(true)
+        //     }).catch(e=>{
+        //         setLogin(false)
+        // })
+
+    }
+    useEffect((e=>{
+
+
+    }),[login])
+
   return (
     <div className="App">
         <Routes>
-            <Route path="/order" element={<Layout/>}/>
+            <Route path="/signup" element={<Signup/>}/>
+            <Route path="/login" element={<Login setLogin={setLogin}/>} />
+            <Route path="/:page" element={login?<Layout/>:<Login setLogin={setLogin}/>}/>
+            <Route path="*" element={<NotFound/>} />
         </Routes>
         {/*After Login Page*/}
-            <Layout />
+        {/*    <Layout />*/}
 
         {/*Login Page*/}
         {/*    <Login/>*/}
         {/*    <Signup/>*/}
+
+        {/*//condition rendering*/}
+        {/*{login?<Layout/>:<Login setLogin={setLogin}/>}*/}
     </div>
   );
 }
