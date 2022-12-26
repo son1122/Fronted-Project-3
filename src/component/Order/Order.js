22807dbb1cedf6de96ad154a87e7d3be73689306
jordanbae <laphon.nala@bumail.net>
2022-12-26 17:46:17 +0700

    Added state for getting menuItems and menuItemsCat

--- src/component/Order/Order.js
+++ src/component/Order/Order.js
@@ -1,14 +1,33 @@
-import "./Order.css"
+import "./Order.css";
 import OrderView from "./OrderView/OrderView";
 import OrderSide from "./OrderSide/OrderSide";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Order = () => {
-    return (
-        <div className={"order-grid"} style={{height:"90vh"}}>
-            <OrderView/>
-            <OrderSide/>
-        </div>
-    );
-}
+  const [menuItems, setMenuItems] = useState([]);
+  const [selectMenuItems, setSelectMenuItems] = useState([]);
+  const [menuItemsCategory, setMenuItemsCategory] = useState("food");
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3001/menu_items/category/${menuItemsCategory}`)
+      .then((res) => {
+        console.log("Response Data from Order.js >> ", res.data);
+        setMenuItems(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [menuItemsCategory]);
+  return (
+    <div className={"order-grid"} style={{ height: "90vh" }}>
+      <OrderView
+        menuItems={menuItems}
+        setMenuItemsCategory={setMenuItemsCategory}
+      />
+      <OrderSide selectMenuItems={selectMenuItems} />
+    </div>
+  );
+};
 
 export default Order;
