ae21d5132550464da6153f0a9bf41b5790a2eb3f
son1122 <wongsatorn.n@hotmail.com>
2022-12-26 12:57:07 +0700

    add order grid

--- src/component/Order/OrderSide/OrderSide.js
+++ src/component/Order/OrderSide/OrderSide.js
@@ -1,8 +1,25 @@
-
+import './OrderSide.css'
 const OrderSide = () => {
     return (
         <div className={"order-side-grid"}>
             <p>Order Side</p>
+            <div className={"order-side-detail-grid"}>
+                <p>Select Table</p>
+                <div>
+                    <div className={"table-side-detail-grid"}>
+                        <p>Fry Rice</p>
+                        <p>1</p>
+                        <p>120</p>
+                    </div>
+                    <div className={"table-side-detail-grid"}>
+                        <p>Fry Rice</p>
+                        <p>1</p>
+                        <p>120</p>
+                    </div>
+                </div>
+                <p>Total</p>
+            </div>
+            <button>Confirm</button>
         </div>
     );
 }
