e9ab825a883be40a81f98d5ee9e0dc5ccf2a9de5
son1122 <wongsatorn.n@hotmail.com>
2022-12-26 17:54:06 +0700

    Update Login.js

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -5,36 +5,38 @@ import axios from "axios";
 
 const Login = (props) => {
     const navigate = useNavigate();
+    const [formData, setFormData] = useState({})
+
+    const handleChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }))
+    }
     const handleSubmit = e => {
-        // e.preventDefault()
-        // let formData = "data-that-send"
-        // axios.post("http://localhost:3001/auth/login", formData)
-        //     .then(res => {
-        //         console.log(res.data);
-        //         let token = res.data
-        //         localStorage.setItem("jwt", token)
-        //     }).catch(err=>{
-        //
-        // })
-        if(username=="admin"&&password=="1234"){
-            props.setLogin(true)
-            navigate('/order')
-        }
+        e.preventDefault()
+        axios.post("http://localhost:3001/auth/login", formData)
+            .then(res => {
+                console.log(res.data);
+                let token = res.data
+                localStorage.setItem("jwt", token)
+                props.setLogin(true)
+                navigate('/order')
+            }).catch(err=>{
+                console.log(err)
+        })
+
     }
 
-    let [username,setUsername]= useState()
-    let [password,setPassword]= useState()
 
     return (
         <div className={"login-grid"}>
             <img style={{height:"40vh"}} src={"https://cdn-icons-png.flaticon.com/512/3135/3135715.png"}/>
             <form>
-                <h2>Username : <input type={"text"} name={"Username"} placeholder={"Username"} onChange={(e)=>{
-                    setUsername(e.target.value)
-                }}/></h2>
+                <h2>Username : <input type={"text"} name={"username"} placeholder={"Username"} onChange={handleChange
+                }/></h2>
                 <br/>
-                <h2>Password : <input type={"text"} name={"Password"} placeholder={"Password"} onChange={(e)=>{
-                    setPassword(e.target.value)}
+                <h2>Password : <input type={"text"} name={"password"} placeholder={"Password"} onChange={handleChange
                 }/></h2>
                 <input type={"button"} onClick={handleSubmit} value={"Login"}/>
             </form>
