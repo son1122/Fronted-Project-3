8bca58f532851e9656bdcd45bdfb9caae0152d54
jordanbae <laphon.nala@bumail.net>
2022-12-29 13:22:37 +0700

    added price and edit button

--- src/component/Order/OrderView/OrderView.js
+++ src/component/Order/OrderView/OrderView.js
@@ -74,34 +74,46 @@ const OrderView = ({
         <img
           key={menuitem.id}
           className="order-view-menu-item"
-          style={{ width: "250px" }}
+          style={{ width: "200px" }}
           src={menuitem.img}
           alt={menuitem.name}
           onClick={() => handleSelectMenuItem(menuitem)}
         />
         <h2>{menuitem.name}</h2>
+        <h2>฿ {menuitem.price}</h2>
       </div>
     );
   });
   return (
     <div className={"order-view-grid"}>
-      <p>Order View</p>
+      <h1 className="view-header">Order View</h1>
       <div className="order-view-search-cont">
         <form className="order-view-search-form">
-          <input
-            type="text"
-            class="search-textbox"
-            placeholder="Search"
-            value={itemsSearchQuery}
-            onChange={handleSearchChange}
-          />
-          <input
-            title="Search"
-            value="⌕"
-            type="submit"
-            class="search-button"
-            onClick={handleSearch}
-          />
+          <div class="searchBar">
+            <input
+              id="searchQueryInput"
+              type="text"
+              name="searchQueryInput"
+              placeholder="Search"
+              value={itemsSearchQuery}
+              onChange={handleSearchChange}
+            />
+            <button
+              id="searchQuerySubmit"
+              type="submit"
+              name="searchQuerySubmit"
+              onClick={handleSearch}
+            >
+              <svg
+                style={{ width: "24px", height: "24px", viewBox: "0 0 24 24" }}
+              >
+                <path
+                  fill="#666666"
+                  d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z"
+                />
+              </svg>
+            </button>
+          </div>
         </form>
       </div>
       <div className={"order-slide-horizon"}>{allMenuItems}</div>
@@ -117,7 +129,7 @@ const OrderView = ({
           </button>
           <button
             className="order-view-category-btn"
-            id="button-31"
+            id="category-bev-btn"
             value="beverage"
             onClick={handleCategoryChange}
           >
