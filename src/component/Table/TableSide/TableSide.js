ec5c095751e35a8186cc05daac2e402dc61d5a97
jordanbae <laphon.nala@bumail.net>
2023-01-01 16:12:38 +0700

    fix table

--- src/component/Table/TableSide/TableSide.js
+++ src/component/Table/TableSide/TableSide.js
@@ -1,12 +1,20 @@
-import "./TableSide.css"
-import React, {useState, useEffect} from "react";
+import "./TableSide.css";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-
-
-const TableSide = ({selTable, order, setOrder, orderDetail}) => {
-
-  const [menuItem , setMenuItem] = useState([])
+const TableSide = ({
+  setSelTable,
+  order,
+  tables,
+  setTables,
+  setOrder,
+  orderDetail,
+  setOrderDetail,
+  selTable,
+  tableOrderDetailState,
+  setTableOrderDetail,
+}) => {
+  const [menuItem, setMenuItem] = useState([]);
 
   useEffect(() => {
     axios
@@ -20,51 +28,54 @@ const TableSide = ({selTable, order, setOrder, orderDetail}) => {
       });
   }, []);
 
-//Map menu API
-let menuDetail = 0
+  //Map menu API
+  let menuDetail = 0;
 
-menuDetail = menuItem.map(item =>{ 
-  return({
+  menuDetail = menuItem.map((item) => {
+    return {
       id: item.id,
-      name : item.name,
-      price : item.price
-    } 
-    )
-})
+      name: item.name,
+      price: item.price,
+    };
+  });
 
-// Map orderdetail API
-  let tableOrderDetail = 0
-try {
-  tableOrderDetail = orderDetail.map(item =>{ 
-    return({
-        id: item.order_id,
-        menuItemId : item.menu_item_id,
-        quantity : item.quantity
-      } 
-      )
-  })
-
-
-} catch (error) {
-
-}
-
-
+  const allOrderDetailDataFromTable = tableOrderDetailState.map((item, key) => {
     return (
-        <div className={"table-side-grid"}>
-            <p>Table Number {selTable}</p>
-
-            <div>
-                <div className={"table-side-detail-grid"}>   
-
-                    <p>Order detail </p>
-                    <p>{menuItem.id}</p>
-                    {console.log(tableOrderDetail , menuDetail)}
-                </div>  
-            </div>
-            <p>Total Price</p>
-            <button>Check Button</button>
-        </div>
+      <div key={key}>
+        <p>Name: {item.name}</p>
+        <p>Price: {item.price}</p>
+        <p>Quantity: {item.quantity}</p>
+      </div>
     );
-}
+  });
+
+  // Map orderdetail API
+  // let tableOrderDetail = 0;
+  // try {
+  //   tableOrderDetail = orderDetail.map((item) => {
+  //     return {
+  //       id: item.order_id,
+  //       menuItemId: item.menu_item_id,
+  //       quantity: item.quantity,
+  //     };
+  //   });
+  // } catch (error) {}
+
+  return (
+    <div className={"table-side-grid"}>
+      <p>Table Number {selTable}</p>
+
+      <div>
+        <div className={"table-side-detail-grid"}>
+          <p>Order detail </p>
+          {allOrderDetailDataFromTable}
+          {/* <p>{menuItem.id}</p> */}
+          {/* {console.log(tableOrderDetail, menuDetail)} */}
+        </div>
+      </div>
+      <p>Total Price</p>
+      <button>Check Button</button>
+    </div>
+  );
+};
 export default TableSide;
