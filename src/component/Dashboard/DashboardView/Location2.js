a5d3762de17a2a56f5130294b487e745f7f57657
son1122 <wongsatorn.n@hotmail.com>
2023-01-03 16:23:25 +0700

    Update Location2.js

--- src/component/Dashboard/DashboardView/Location2.js
+++ src/component/Dashboard/DashboardView/Location2.js
@@ -1,19 +1,44 @@
 
-import React from "react";
+import React, {useEffect} from "react";
 import GoogleMapReact from 'google-map-react';
+import axios from "axios";
 
 const AnyReactComponent = ({ text }) => <div>
 <img style={{width:"50px"}} src={"https://static.thenounproject.com/png/2900961-200.png"}/>
     {text}
 </div>;
 
+const Restaurant = ({ text }) => <div>
+    <img style={{width:"50px"}} src={"https://thumbs.dreamstime.com/b/simple-restaurant-icon-can-flat-design-restaurant-icon-vector-restaurant-symbol-130849249.jpg"}/>
+    {text}
+</div>;
+
 export default function Location(){
+    useEffect(()=>{
+        axios.get("http://localhost:3001/location/seller", {
+                headers: { Authorization: `Bearer ${localStorage.getItem("jwt")}` },
+            })
+            .then((resSeller) => {
+
+            }).catch(e=>{
+
+        })
+        axios.get("http://localhost:3001/location/customer", {
+                headers: { Authorization: `Bearer ${localStorage.getItem("jwt")}` },
+            })
+            .then((resCustomer) => {
+
+            }).catch(e=>{
+
+        })
+    },[])
     const defaultProps = {
         center: {
-            lat: 59.99835602,
-            lng: 30.01502627
+            lat: 13.685321440355466,
+            lng: 100.61134873568787
+            // 13.685321440355466, 100.61134873568787
         },
-        zoom: 11
+        zoom: 14
     };
 
     return (
@@ -24,12 +49,13 @@ export default function Location(){
                 defaultCenter={defaultProps.center}
                 defaultZoom={defaultProps.zoom}
             >
-                <AnyReactComponent
-                    lat={59.955413}
-                    lng={30.337844}
-                    text="My Marker"
-                />
-                
+
+                <Restaurant
+                    lat={13.685321440355466}
+                    lng={100.61134873568787}
+                    text="Restaurant "
+                    />
+
             </GoogleMapReact>
         </div>
     );
